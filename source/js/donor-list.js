157a6be542e77af5f4c2593c90f13c18cb119f66
Jason Mickela <jason@rootid.in>
2018-08-22 21:58:13 -0700

    donor list updates

--- source/js/donor-list.js
+++ source/js/donor-list.js
@@ -137,3 +137,82 @@ function updateIndividualColumns(filterVals = {lower: undefined, upper: undefine
     contain: true,
   });
 }
+
+function updateInstitutionalColumns(filterVals = { lower: undefined, upper: undefined, text: undefined }) {
+  let itemCount = 20;
+
+  if (window.innerWidth > 982) {
+    itemCount = 40;
+  } else if (window.innerWidth > 600) {
+    itemCount = 30;
+  }
+
+  var names = new Array();
+
+  for (var i = 0; i < 300; i++) {
+    names.push({
+      name: "Jason Mickela Corp " + i,
+      amount: i * 20
+    });
+  }
+
+  names.push({
+    name: "Marcia Adelman",
+    amount: i * 20
+  });
+  names.push({
+    name: "Craig Adelman",
+    amount: i * 20
+  });
+  names.push({
+    name: "Anonymous (3)",
+    amount: i * 20
+  });
+
+  if (filterVals.lower !== undefined) {
+    names = names.filter(function (item) {
+      return item.amount >= filterVals.lower;
+    });
+  }
+
+  if (filterVals.upper !== undefined) {
+    names = names.filter(function (item) {
+      return item.amount <= filterVals.upper;
+    });
+  }
+
+  if (filterVals.text !== undefined) {
+    names = names.filter(function (item) {
+
+      return item.name.includes(filterVals.text);
+    });
+  }
+
+  // If the slider is already enabled, destroy it.
+  if ($('.donorlist__institution__cols__wrapper').hasClass('flickity-enabled')) {
+    $('.donorlist__institution__cols__wrapper').flickity('destroy');
+  }
+
+  // Remove any existing children
+  $('.donorlist__institution__cols__wrapper').empty();
+
+  // Break array into max num chunks
+
+
+  for (var j = 0; j < names.length / 10; j++) {
+    var container = document.createElement('div');
+    container.className = "donorlist__institution__col";
+
+    $('.donorlist__institution__cols__wrapper').append(container);
+
+    for (var i = 0; i < itemCount && names.length; i++) {
+      var name = names.pop();
+      $(container).append("<div>" + name.name + "</div>");
+    }
+  }
+
+  $('.donorlist__institution__cols__wrapper').flickity({
+    cellAlign: 'left',
+    contain: true,
+  });
+}
