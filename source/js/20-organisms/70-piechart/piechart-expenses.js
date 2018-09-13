f13aac58a623023c652b9e00174fe506c264c7fd
Meg Claypool <margaret.claypool@gmail.com>
2018-09-13 00:13:46 -0700

    Animate piecharts when visible, not on page load

--- source/js/20-organisms/70-piechart/piechart-expenses.js
+++ source/js/20-organisms/70-piechart/piechart-expenses.js
@@ -7,49 +7,71 @@ $(document).ready(function(){
     let color_dusk_blue = '38,77,158'; // #264d9e
     let color_white = '255,255,255'; // #ffffff
     
-  
-    if(document.getElementById("expensesChart")) {
-      new Chart(document.getElementById("expensesChart"), {
-        type: 'pie',
-        data: {
-          // These labels appear in the legend and in the tooltips when hovering different arcs
-          labels: ["SERVICES TO MAINTAIN OUR BUILDINGS", "SERVICES TO SUPPORT RESIDENTS", "HOUSING DEVELOPMENT", "ADMINISTRATION", "FUNDRAISING"],
-          datasets: [{
-            data: [7161528.6, 3501191.76, 2705466.36, 2068886.04, 954870.48],
-            backgroundColor: ["rgb("+color_tomato+")", "rgb("+color_yellow_tan+")", "rgb("+color_turquoise_blue+")", "rgb("+color_hospital_green+")", "rgb("+color_dusk_blue+")"], 
-            
-            borderColor: ["rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)"],
-  
-            borderWidth: 6,
-            
-            hoverBackgroundColor: ["rgba("+color_tomato+",.8)", "rgba("+color_yellow_tan+",.8)", "rgba("+color_turquoise_blue+",.8)", "rgba("+color_hospital_green+",.8)", "rgba("+color_dusk_blue+",.8)"],
-  
-            hoverBorderColor: ["rgba("+color_tomato+",.8)", "rgba("+color_yellow_tan+",.8)", "rgba("+color_turquoise_blue+",.8)", "rgba("+color_hospital_green+",.8)", "rgba("+color_dusk_blue+",.8)"],
-                  
-          }],
-        },
-        options: {
-          title: {
-            display: false,
-            text: 'Total Revenue'
-          },
-          maintainAspectRatio: false,
-          responsive: true,
+
+    
+    let expensesChartData = {
+      type: 'pie',
+      data: {
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: ["SERVICES TO MAINTAIN OUR BUILDINGS", "SERVICES TO SUPPORT RESIDENTS", "HOUSING DEVELOPMENT", "ADMINISTRATION", "FUNDRAISING"],
+        datasets: [{
+          data: [7161528.6, 3501191.76, 2705466.36, 2068886.04, 954870.48],
+          backgroundColor: ["rgb("+color_tomato+")", "rgb("+color_yellow_tan+")", "rgb("+color_turquoise_blue+")", "rgb("+color_hospital_green+")", "rgb("+color_dusk_blue+")"], 
           
-          legend: {
-            display: false,
-            position: 'bottom',
-            labels: {
-                boxWidth: 15,
-                fontColor: 'rgb(10, 10, 10)',
-                fontSize: 13,
-                padding: 30,
-            },
-          },
-          tooltips: {
-            enabled: false,
+          borderColor: ["rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)"],
+
+          borderWidth: 6,
+          
+          hoverBackgroundColor: ["rgba("+color_tomato+",.8)", "rgba("+color_yellow_tan+",.8)", "rgba("+color_turquoise_blue+",.8)", "rgba("+color_hospital_green+",.8)", "rgba("+color_dusk_blue+",.8)"],
+
+          hoverBorderColor: ["rgba("+color_tomato+",.8)", "rgba("+color_yellow_tan+",.8)", "rgba("+color_turquoise_blue+",.8)", "rgba("+color_hospital_green+",.8)", "rgba("+color_dusk_blue+",.8)"],
+                
+        }],
+      },
+      options: {
+        title: {
+          display: false,
+          text: 'Total Revenue'
+        },
+        maintainAspectRatio: false,
+        responsive: true,
+        
+        legend: {
+          display: false,
+          position: 'bottom',
+          labels: {
+              boxWidth: 15,
+              fontColor: 'rgb(10, 10, 10)',
+              fontSize: 13,
+              padding: 30,
           },
         },
-      });
+        tooltips: {
+          enabled: false,
+        },
+      },
+    };
+
+    let inView = false;
+
+    function isScrolledIntoView(elem)
+    {
+      let docViewTop = $(window).scrollTop();
+      let docViewBottom = docViewTop + $(window).height();
+  
+      let elemTop = $(elem).offset().top;
+      let elemBottom = elemTop + $(elem).height();
+  
+      return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
     }
+  
+    $(window).scroll(function() {
+      if (isScrolledIntoView('#expensesChart')) {
+        if (inView) { return; }
+        inView = true;
+        new Chart(document.getElementById("expensesChart"), expensesChartData);
+      } else {
+        inView = false;  
+      }
+    });
 });
--- source/js/20-organisms/70-piechart/piechart-revenue.js
+++ source/js/20-organisms/70-piechart/piechart-revenue.js
@@ -8,45 +8,69 @@ $(document).ready(function(){
   let color_white = '255,255,254'; // #fffffe
   
 
-  if (document.getElementById("revenueChart")) {
-    new Chart(document.getElementById("revenueChart"), {
-      type: 'pie',
-      data: {
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: ["GOVERNMENT CONTRACTS", "INDIVIDUAL GIFTS", "EVENTS", "OTHER", "MANAGEMENT FEES", "DEVELOPER FEES", "RENT", "FOUNDATIONS & CORPORATIONS"],
-        datasets: [{
-          data: [1218648.15, 731188.89, 487459.26, 487459.26, 6580700.01, 10967833.35, 2681025.93, 1218648.15],
-          backgroundColor: ["rgb("+color_dusk_blue+")", "rgb("+color_hospital_green+")", "rgb("+color_white+")", "rgb("+color_yellow_tan+")", "rgb("+color_tomato+")", "rgb("+color_yellow_tan+")", "rgb("+color_turquoise_blue+")", "rgb("+color_faded_red+")"], 
-          borderColor: ["rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)"],
-          borderWidth: 6,
-          hoverBackgroundColor: ["rgb("+color_dusk_blue+",.8)", "rgb("+color_hospital_green+",.8)", "rgb(200,200,200,.05)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_tomato+",.8)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_turquoise_blue+",.8)", "rgb("+color_faded_red+",.8)"],
-          hoverBorderColor: ["rgb("+color_dusk_blue+",.8)", "rgb("+color_hospital_green+",.8)", "rgb(200,200,200,.05)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_tomato+",.8)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_turquoise_blue+",.8)", "rgb("+color_faded_red+",.8)"],
-                
-        }],
+  let revenueChartData = {
+    type: 'pie',
+    data: {
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: ["GOVERNMENT CONTRACTS", "INDIVIDUAL GIFTS", "EVENTS", "OTHER", "MANAGEMENT FEES", "DEVELOPER FEES", "RENT", "FOUNDATIONS & CORPORATIONS"],
+      datasets: [{
+        data: [1218648.15, 731188.89, 487459.26, 487459.26, 6580700.01, 10967833.35, 2681025.93, 1218648.15],
+        backgroundColor: ["rgb("+color_dusk_blue+")", "rgb("+color_hospital_green+")", "rgb("+color_white+")", "rgb("+color_yellow_tan+")", "rgb("+color_tomato+")", "rgb("+color_yellow_tan+")", "rgb("+color_turquoise_blue+")", "rgb("+color_faded_red+")"], 
+        borderColor: ["rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)", "rgb(0, 0, 0)"],
+        borderWidth: 6,
+        hoverBackgroundColor: ["rgb("+color_dusk_blue+",.8)", "rgb("+color_hospital_green+",.8)", "rgb(200,200,200,.05)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_tomato+",.8)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_turquoise_blue+",.8)", "rgb("+color_faded_red+",.8)"],
+        hoverBorderColor: ["rgb("+color_dusk_blue+",.8)", "rgb("+color_hospital_green+",.8)", "rgb(200,200,200,.05)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_tomato+",.8)", "rgb("+color_yellow_tan+",.8)", "rgb("+color_turquoise_blue+",.8)", "rgb("+color_faded_red+",.8)"],
+              
+      }],
+    },
+    options: {
+      title: {
+        display: false,
+        text: 'Total Revenue'
       },
-      options: {
-        title: {
-          display: false,
-          text: 'Total Revenue'
-        },
-        maintainAspectRatio: false,
-        responsive: true,
-        
-        legend: {
-          display: false,
-          position: 'bottom',
-          labels: {
-              boxWidth: 15,
-              fontColor: 'rgb(10, 10, 10)',
-              fontSize: 13,
-              padding: 30,
-          },
-        },
-        tooltips: {
-          enabled: false,
+      maintainAspectRatio: false,
+      responsive: true,
+      
+      legend: {
+        display: false,
+        position: 'bottom',
+        labels: {
+            boxWidth: 15,
+            fontColor: 'rgb(10, 10, 10)',
+            fontSize: 13,
+            padding: 30,
         },
       },
-    });
+      tooltips: {
+        enabled: false,
+      },
+    },
+  };
+
+  let inView = false;
+
+  function isScrolledIntoView(elem)
+  {
+    let docViewTop = $(window).scrollTop();
+    let docViewBottom = docViewTop + $(window).height();
+
+    let elemTop = $(elem).offset().top;
+    let elemBottom = elemTop + $(elem).height();
+
+    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
   }
+
+  $(window).scroll(function() {
+    if (isScrolledIntoView('#revenueChart')) {
+      if (inView) { return; }
+      inView = true;
+      new Chart(document.getElementById("revenueChart"), revenueChartData);
+    } else {
+      inView = false;  
+    }
+  });
+
 });
+
+
 
